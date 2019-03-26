f640c86616fdfa75f7f6eab65ef668f9490cab17
junior2355 <43444471+junior2355@users.noreply.github.com>
2019-03-26 09:42:26 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$(".school").click(function(){
+     $(".harry").toggle(); 
+});
+
+
+
+//Spell #2
+
+$(".student").click(function(){
+      $(".ron").toggle(); 
+});
+
+
+
+//Spell #3
+
+$(".dude").click(function(){
+    $(".sick").toggle();
+});
+
+
+
